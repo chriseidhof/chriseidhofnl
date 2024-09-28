e5515ef716748cc8196f9040d4b104ff4a812091
Chris Eidhof <chris@eidhof.nl>
2024-09-28 21:30:34 +0200

    headings

--- Sources/Chris/AboutMe.swift
+++ Sources/Chris/AboutMe.swift
@@ -27,6 +27,8 @@ struct AboutMe: Rule {
         I started my career building websites with PHP, HTML and CSS (actually, CSS wasn't a big thing yet back then). I moved on to writing lots of Haskell (in uni) and Javascript (although I hardly recognize today's Javascript). Towards the end of my uni career iOS came out and I started building iOS apps using the very first public SDK.
 
         In 2012, I moved from The Netherlands to Berlin. I was frustrated by the lack of English-speaking iOS conferences I could attend, so together with Matt and Peter we organized [UIKonf](https://www.uikonf.com). This was a big deal for me personally, and seeing the first edition was pure magic. Many friendships (and more!) have started during the two editions of UIKonf that we organized, and I'm really proud to have played a part in that.
+        
+        ## objc.io
 
         After the first edition of UIKonf, Florian, Daniel and me started [objc.io](https://www.objc.io). We came up with the idea while sitting outside during the hack day following the conference. At first, we put out a [monthly magazine](https://www.objc.io/issues/) with many amazing contributors.
 
@@ -37,12 +39,19 @@ struct AboutMe: Rule {
         In 2024 I created the [SwiftUI Field Guide](https://www.swiftuifieldguide.com) as a way to visually teach how SwiftUI's layout system works.
 
         I enjoy running, cycling and spending lots of time with my young kids. I like building physical things (I'm not very good at it, but I really enjoy woodworking).
+        
+        ## Conferences
 
-        I enjoy speaking at conferences. Here's an annotated version of my talk [A Day in the Life of a SwiftUI View](/presentations/day-in-the-life).
+        I enjoy speaking at conferences. Here are two recent talks:
+        
+        - [A Day in the Life of a SwiftUI View](/presentations/day-in-the-life) (Toronto, 2023)
+        - [SwiftUI Animations](/presentations/swiftui-animations) (Paris, 2024)
+        
+        I'll try to add more presentations here in the future.
 
-        Here's a bio for conferences:
-
-        > Chris is the founder of [objc.io](https://www.objc.io) and creator of [SwiftUI Field Guide](https://www.swiftuifieldguide.com/). He wrote multiple books about Swift, runs the [Swift Talk](https://www.objc.io) video series and organizes SwiftUI workshops.
+        ## Bio
+        
+        > Chris is the founder of [objc.io](https://www.objc.io) and creator of [SwiftUI Field Guide](https://www.swiftuifieldguide.com/). He wrote multiple books about Swift, runs the [Swift Talk](https://www.objc.io) video series and organizes [SwiftUI workshops](https://swiftuifieldguide.com/workshops/).
         
         ![](/images/chris.jpg)
         
